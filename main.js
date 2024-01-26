166c0177bb156bd55ace8f5eb60f7c8346381f49
Yuri Cardozo <103226844+Netunosz@users.noreply.github.com>
2024-01-25 21:20:16 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+}
